c94c5fd5c5e0f51a3a7a67bda77218e7e9ae0148
Lucas <lucasoliveirars22@gmail.com>
2024-08-17 21:24:55 -0300

    base oauth google login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GitHub } from "@mui/icons-material";
+import { Email, GitHub } from "@mui/icons-material";
 import Logo from "/public/popcorn-logo.svg";
 import Image from "next/image";
 
@@ -11,24 +11,38 @@ export default function Login() {
 
   const handleGithubLogin = async () => {
     const clientID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
-    const callbackURL = process.env.NEXT_PUBLIC_GITHUB_CALLBACK;
+    const callbackURL = `${process.env.NEXT_PUBLIC_GITHUB_CALLBACK}?provider=github`;
     window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${callbackURL}`;
+  };
+
+  const handleGoogleLogin = async () => {
+    const clientID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+    const callbackURL = process.env.NEXT_PUBLIC_GOOGLE_CALLBACK;
+    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${callbackURL}&prompt=consent&response_type=code&client_id=${clientID}&scope=openid%20email%20profile&access_type=offline`;
   };
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get("code");
+    const provider = urlParams.get("provider");
 
     if (code) {
       const fetchData = async () => {
         try {
-          const response = await baseAPI.post("/auth/dj-rest-auth/github/", {
-            code: code,
-          });
-
-          const access_token = response.data;
-          localStorage.setItem("access_token", access_token.token);
-
+          let response;
+          if (provider === "github") {
+            response = await baseAPI.post("/auth/dj-rest-auth/github/", {
+              code: code,
+            });
+          } else {
+            response = await baseAPI.post("/auth/dj-rest-auth/google/", {
+              code: code,
+            });
+          }
+          if (response?.status === 200 && response.data) {
+            const access_token = response.data;
+            localStorage.setItem("access_token", access_token.token);
+          }
           router.push("/home");
         } catch (error) {
           console.error("Erro ao fazer a requisição:", error);
@@ -54,6 +68,14 @@ export default function Login() {
       >
         <GitHub sx={{ fontSize: 20, lineHeight: 20 }} />
         Github
+      </button>
+
+      <button
+        onClick={handleGoogleLogin}
+        className="w-36 h-12 bg-gray-50 mt-4 rounded-xl"
+      >
+        <Email sx={{ fontSize: 20, lineHeight: 20 }} />
+        Gmail
       </button>
     </main>
   );
